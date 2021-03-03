1ac1c2c61970c020056d638d84a3d6f05f977665
Danny Campbell <daniel.campbell@duke.edu>
2021-03-03 14:07:53 -0500

    non-functional, clickable calendar

--- my-app/src/pages/PatientHub/InfoFolder/PatientDiary.js
+++ my-app/src/pages/PatientHub/InfoFolder/PatientDiary.js
@@ -14,16 +14,12 @@ class PatientDiary extends React.Component {
 
   render() {
     return (
-      <Col>
+      <Col> 
       <h3 className="mt-5">Calendar</h3>
       <h1>{this.props.patient.name}</h1>
        <FullCalendar
         plugins={[ dayGridPlugin, interactionPlugin ]}
         initialView="dayGridMonth"
-        events={[
-          { title: 'Diary Entry', date: '2021-03-03' },
-          { title: 'Diary Entry', date: '2021-03-05' }
-        ]}
         selectable = {true}
       />
       </Col>
