8fb720a4dce2751a84af69ba9f95d430c54fcfc5
Danny Campbell <daniel.campbell@duke.edu>
2021-03-03 00:02:17 -0500

    added events

--- my-app/src/pages/PatientHub/InfoFolder/PatientDiary.js
+++ my-app/src/pages/PatientHub/InfoFolder/PatientDiary.js
@@ -3,6 +3,9 @@
 import React from "react";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import Calendar from 'react-calendar'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction';
 
 class PatientDiary extends React.Component {
   constructor(props) {
@@ -14,9 +17,15 @@ class PatientDiary extends React.Component {
       <Col>
       <h3 className="mt-5">Calendar</h3>
       <h1>{this.props.patient.name}</h1>
-      <div>
-        <Calendar></Calendar>
-      </div>
+       <FullCalendar
+        plugins={[ dayGridPlugin, interactionPlugin ]}
+        initialView="dayGridMonth"
+        events={[
+          { title: 'Diary Entry', date: '2021-03-03' },
+          { title: 'Diary Entry', date: '2021-03-05' }
+        ]}
+        selectable = {true}
+      />
       </Col>
     );
   }
